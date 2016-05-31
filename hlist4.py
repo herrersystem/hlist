a6750321e54d0ced5c185f04183850826bf6f952
Anton Millescamps <anton.millescamps@gmail.com>
2016-05-31 08:21:45 +0200

    opt code

--- hlist4.py
+++ hlist4.py
@@ -9,25 +9,33 @@ https://github.com/krig/send_arp.py
 import socket, codecs, os
 import argparse
 
-__author__="HerrerSystem"
 
 def conversionHexIP(adresse):
 	adresseHex=str()
+	
 	for element in adresse.split('.'): 
 		tmp=str(hex(int(element)))[2:]
-		if len(tmp) < 2: tmp='0'+tmp
+		
+		if len(tmp) < 2: 
+			tmp='0'+tmp
+		
 		adresseHex+=tmp
 	
 	return adresseHex
 	
+	
 def conversionGraphMac(adresse):
 	return adresse[:2]+':'+adresse[2:4]+':'+adresse[4:6]+':'+adresse[6:8]+':'+adresse[8:10]+':'+adresse[10:12]
 
+
 def conversionHexMac(adresse):
 	adresseHex=str()
-	for element in adresse.split(':'): adresseHex+=element
+	
+	for element in adresse.split(':'): 
+		adresseHex+=element
 	
 	return adresseHex
+
 
 def ethernet(src, dst):
 	dst=conversionHexMac(dst)
@@ -35,6 +43,7 @@ def ethernet(src, dst):
 	protocole='0806' #Protocole ARP.
 	
 	return dst+src+protocole
+
 
 def arp(hsrc, src, dst):
 	htype='0001'
@@ -49,6 +58,7 @@ def arp(hsrc, src, dst):
 	
 	return htype+ptype+hlenght+plenght+operation+hsrc+psrc+hdst+pdst
 
+
 def monAdresseIP(interface):
 	ipconfig=os.popen('ifconfig %s'%interface).readlines()
 	motcle='inet adr:'
@@ -59,9 +69,12 @@ def monAdresseIP(interface):
 	if indice != -1 and indice2 != -1: 
 		indice+=len(motcle)
 		padresse=ipconfig[1][indice:indice2-1]
-	else: padresse=False
+		
+	else: 
+		padresse=False
 	
 	return padresse
+	
 	
 def monAdresseMac(interface):
 	ipconfig=os.popen('ifconfig %s'%interface).readlines()
@@ -71,9 +84,12 @@ def monAdresseMac(interface):
 	if indice != -1: 
 		indice+=len(motcle)
 		hadresse=ipconfig[0][indice:indice+17]
-	else: hadresse=False
+		
+	else: 
+		hadresse=False
 	
 	return hadresse
+
 
 def listeAdresse(fourchette):
 	liste=list()
@@ -83,16 +99,26 @@ def listeAdresse(fourchette):
 		tmp=tmp.split('.')
 		statique="{}.{}.{}.".format(tmp[0], tmp[1], tmp[2])
 		borneMin=tmp[3]
-		if int(borneMin) > int(borneMax): liste.append(False)
-		else:
-			for i in range(int(borneMin), int(borneMax)+1): liste.append(statique+str(i))
 		
-	elif len(fourchette.split('-')) == 1: liste.append(fourchette)
-	else: liste.append(False)
+		if int(borneMin) > int(borneMax): 
+			liste.append(False)
+			
+		else:
+			for i in range(int(borneMin), int(borneMax)+1): 
+				liste.append(statique+str(i))
+		
+	elif len(fourchette.split('-')) == 1: 
+		liste.append(fourchette)
+		
+	else: 
+		liste.append(False)
 	
 	return liste
 
+
 def envoiRequete(interface, liste, maMac, monIp):
+	global parser
+	
 	if liste[0] != False:
 		s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW)
 		s.settimeout(2)
@@ -111,19 +137,31 @@ def envoiRequete(interface, liste, maMac, monIp):
 					proto=rep.decode()[24:28]
 					op=rep.decode()[43]
 					ip=rep.decode()[56:64]
+					
 					if proto == "0806" and op == '2' and ip == conversionHexIP(cible): 
-						print("> [{}] {} ({})".format(socket.gethostbyaddr(cible)[0], cible, conversionGraphMac(mac)))
+						print("> [{}] {} ({})".format(
+							socket.gethostbyaddr(cible)[0], cible, conversionGraphMac(mac))
+						)
 						
-			except socket.timeout: pass
-			except socket.gaierror: parser.print_help()
+			except socket.timeout: 
+				pass
+				
+			except socket.gaierror: 
+				parser.print_help()
 		
 		s.close()
-	else: parser.print_help()
+		
+	else: 
+		parser.print_help()
+
 
 if __name__ == '__main__':
-	parser=argparse.ArgumentParser(prog='hlist4', description='Découverte d\'hôte via ARP')
-	parser.add_argument('-i', action='store', metavar='interface', required=True, help='Interface à utiliser')
-	parser.add_argument('-f', action='store', metavar='fourchette', required=True, help='Adresse(s) à scanner (ex: 192.168.1.21-52 ou 192.168.1.21)')
+	parser=argparse.ArgumentParser(prog='hlist4', 
+		description='Découverte d\'hôte via ARP')
+	parser.add_argument('-i', action='store', metavar='interface', required=True, 
+		help='Interface à utiliser')
+	parser.add_argument('-f', action='store', metavar='fourchette', required=True, 
+		help='Adresse(s) à scanner (ex: 192.168.1.21-52 ou 192.168.1.21)')
 	arg=parser.parse_args()
 	
 	interface=arg.i
@@ -131,6 +169,9 @@ if __name__ == '__main__':
 	maMac=monAdresseMac(interface)
 	monIp=monAdresseIP(interface)
 	
-	if maMac == False or monIp == False: print("> [ERR] interface %s non trouvée"%interface) 
-	else: envoiRequete(interface, liste, maMac, monIp)
+	if not maMac or not monIp: 
+		print("> [ERR] interface %s non trouvée"%interface) 
+		
+	else: 
+		envoiRequete(interface, liste, maMac, monIp)
 	
